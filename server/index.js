edeed009289b5866bfc7a44e894fd302e723fa6f
Farhod <mrfarhod58@gmail.com>
2023-03-08 11:39:00 +0500

    connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express")
 const app = express()
 const PORT = 5000
+const {MONGO_URI} = require("./keys")
+const mongoose = require("mongoose")
 
 
 app.get("/", (req, res)=>{
     res.send("ura")
+})
+
+mongoose.set('strictQuery', false);
+mongoose.connect(MONGO_URI)
+
+mongoose.connection.on("connected", ()=>{
+    console.log("MongoDB is connected");
+})
+mongoose.connection.on("error", (err)=>{
+    console.log("MongoDB is not working", err);
 })
 
 app.listen(PORT, ()=>{
