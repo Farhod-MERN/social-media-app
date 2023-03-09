9fe5357caf25234a72ac90708e7a28bae131e856
Farhod <mrfarhod58@gmail.com>
2023-03-09 15:41:52 +0500

    JWT

---
+++ server/middleware/login.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require('../keys/index')
+const mongoose = require("mongoose")
+const User = mongoose.model("User")
+
+module.exports = (req, res, next) => {
+    const {authorization} = req.headers
+
+    if(!authorization){
+        res.status(401).json({"error": "Please Sign in"})
+    }
+    const token = authorization.replace("Farhod ", "")
+    jwt.verify(token, JWT_SECRET, (err, payload)=>{
+        if(err){
+           return res.status(401).json({"error" : "Error occured"}) 
+        }
+        const {_id} = payload
+        User.findById(_id).then(userData =>{
+            req.user = userData
+        })
+        next()
+    } )
+};
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -2,7 +2,11 @@ const { Router } = require("express");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const router = Router();
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../keys/index");
+const loginMiddleware = require("../middleware/login")
+
 
 router.get("/", (req, res) => {
   res.send("Hello");
@@ -22,36 +26,47 @@ router.post("/signup", (req, res) => {
           .json({ error: "User already exsist with that email" });
       }
 
-     bcrypt.hash(password, 10).then(hashPass=>{
-         const user = User.create({ name, password: hashPass, email });
-     })
+      bcrypt.hash(password, 10).then((hashPass) => {
+        const user = User.create({ name, password: hashPass, email });
+      });
     })
     .then((user) => {
-      res.json({ "msg": "User is added successfully" });
-    }).catch(err =>{
-        console.log(err);
+      res.json({ msg: "User is added successfully" });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
-router.post("/signin", (req, res)=>{
-    const {email, password} = req.body
-    if(!email || !password){
-      return res.status(422).json({"error": "please enter both your email and password"})
-    }
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .json({ error: "please enter both your email and password" });
+  }
 
-    User.findOne({email: email}).then((savedUser)=>{
-      if(!savedUser){
-        res.status(422).json({"error": "User not found"})
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        res.status(422).json({ error: "User not found" });
       }
-      bcrypt.compare(password, savedUser.password).then((isEqual)=>{
-        if(!isEqual){
-         return res.status(422).json({"error": "Password is wrong"})
-        }else{
-          res.json({"msg": "Successfully signed in"})
+      bcrypt.compare(password, savedUser.password).then((isEqual) => {
+        if (!isEqual) {
+          return res.status(422).json({ error: "password is wrong" });
+        } else {
+          const token = jwt.sign({_id : savedUser._id}, JWT_SECRET, {expiresIn: "30d"})
+          res.json({token: token});
         }
-      })
-    }).catch(err =>{
-      console.log(err);
+      });
     })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/protected", loginMiddleware,(req, res)=>{
+  res.send("Protected user")
 })
+
 module.exports = router;
